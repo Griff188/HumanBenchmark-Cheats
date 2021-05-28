47a74a46c972d2f50f2a78dd89a561df27f7fec7
Griff188 <83679430+Griff188@users.noreply.github.com>
2021-05-28 11:11:52 -0400

    Update HumanBenchmark-typing-v2.py

--- HumanBenchmark-typing-v2.py
+++ HumanBenchmark-typing-v2.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 browser = webdriver.Chrome('\chromedriver.exe')
 browser.get('https://www.humanbenchmark.com/tests/typing')
 
-time.sleep(20)
+input("ready? ")
 
 element = browser.find_element_by_xpath('/html/body/div[1]/div/div[4]/div[1]/div/div[2]/div')
 letters = browser.find_elements_by_xpath('/html/body/div[1]/div/div[4]/div[1]/div/div[2]/div')
