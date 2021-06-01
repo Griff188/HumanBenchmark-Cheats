a68389620b3108d13e31bd521852dfdb87498a2a
Griff188 <83679430+Griff188@users.noreply.github.com>
2021-05-31 21:30:51 -0400

    Update HumanBenchmark-visual.py

--- HumanBenchmark-visual.py
+++ HumanBenchmark-visual.py
@@ -3,19 +3,22 @@ from pynput.mouse import Button, Controller
 import time
 from selenium import webdriver
 
+# enter what level to go to
 limit = input("go to level: ")
 counter = 0
 
+# default to level 20
 try:
     limit = int(limit)
 except:
     limit = 20
 
+# print target level
 print("going to", limit)
 
+# define location of chrome driver and open new window to visual memory test
 driver = webdriver.Chrome('\chromedriver.exe')
 driver.get('https://www.humanbenchmark.com/tests/memory')
-
 driver.maximize_window()
 
 # time to sign-in
@@ -24,11 +27,16 @@ input("ready? ")
 mouse = Controller()
 image = None
 
+# identify start button
 start = driver.find_element_by_xpath('/html/body/div[1]/div/div[4]/div[1]/div/div/div/div[2]/button')
 
+# for first run
 if counter == 0:
+    # mouse to center of the screen
     mouse.position = (440, 460)
     time.sleep(1)
+    
+    # click start
     start.click()
     counter += 1
 
@@ -42,7 +50,7 @@ for level in range(0, limit):
 
     time.sleep(1.1)
 
-    # go through x, y
+    # go through x, y coords
     for xSize in range(0, 400, 20):
         for ySize in range(0, 400, 20):
 
